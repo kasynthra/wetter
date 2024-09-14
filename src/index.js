628b7a76fdb2f73ffa8106f7410259c1b81f5daa
kasynthra <139115720+kasynthra@users.noreply.github.com>
2024-09-15 00:30:04 +0200

    Injected Forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -52,7 +52,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="forecast-day">
+            <div class="forecast-date">${day}</div>
+            <div class="forecast-icon">⛅</div>
+            <div class="forecast-temperatures">
+              <div class="forecast-temperature">
+                <strong>15°</strong>
+              </div>
+              <div class="forecast-temperature">9°</div>
+            </div>
+          </div>
+          `;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Tokyo");
+displayForecast();
